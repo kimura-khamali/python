f377e5bdf534a943deab3ceba4b2e09953f5f811
kimura-khamali <kimurakhamali@gmail.com>
2024-02-27 08:20:33 +0300

    python file

---
+++ anness.py
@@ -0,0 +1,32 @@
+
+print("select option")
+print("1. ADD")
+print("2. SUBTRACT")
+print("3. MULIPLY")
+print("4. DIVIDE")
+
+
+
+operation = input()
+if operation == "1":
+    num1 = input("enter first number:")
+    num2 = input("enter second number:")
+    print("the sum is: " + str(int(num1) + int(num2)))
+elif operation == "2":
+    num1 = input("enter first number:")
+    num2 = input("enter second number:")
+    print("the difference is: " + str(int(num1) - int(num2)))
+elif operation == "3":
+    num1 = input("enter first number:")
+    num2 = input("enter second number:")
+    print("the product is: " + str(int(num1) * int(num2)))
+elif operation == "4":
+    num1 = input("enter first number:")
+    num2 = input("enter second number:")
+    print("the division is: " + str(int(num1) / int(num2)))
+    
+
+
+
+
+
